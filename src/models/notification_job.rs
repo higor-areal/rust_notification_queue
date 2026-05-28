da31227784ed0ffe08902d40818a1f5e66f3a748
Esau Higor <esauhigor@gmail.com>
2026-05-28 17:55:38 -0400

    criando função de valid

--- src/models/notification_job.rs
+++ src/models/notification_job.rs
@@ -1,6 +1,37 @@
+use uuid::Uuid;
+use serde::Deserialize;
+
+#[derive(Clone)]
 pub struct NotificationJob {
     pub id: String,
     pub notification_type: String,
     pub to: String,
     pub message: String,
+}
+
+
+impl NotificationJob{
+    pub fn new(not: &str, to: &str, msg: &str) -> Self{
+        Self { 
+            id: Uuid::new_v4().to_string(),
+            notification_type: not.to_string(),
+            to: to.to_string(),
+            message: msg.to_string() }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct NotificationJobRequest {
+    pub notification_type: String,
+    pub to: String,
+    pub message: String,
+}
+
+impl NotificationJobRequest{
+    pub fn valid(&self) -> bool {
+        if self.message.is_empty() || self.to.is_empty() {
+            return false;
+        }
+        true
+    }
 }
